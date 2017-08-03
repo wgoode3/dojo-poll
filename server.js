8fe182233d96704a5a31314a8a8232c321df870c
wgoode3 <wgoode3@gmail.com>
2017-08-03 16:48:50 -0400

    cleaned up the server a bit

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 "use strict"
-var express = require("express");
-var path = require("path");
-var app = express();
-var port = 8000;
-
-const cookieParser = require('cookie-parser');
-
-const jwt = require('jsonwebtoken');
-const uuidv4 = require('uuid/v4');
-
-const {TOKEN_SECRET} = require('./config');
+var express 			= require("express");
+var path 				= require("path");
+var app 				= express();
+var port 				= 8000;
+const cookieParser 		= require('cookie-parser');
+const jwt 				= require('jsonwebtoken');
+const uuidv4 			= require('uuid/v4');
+const {TOKEN_SECRET} 	= require('./config');
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "static")));
@@ -20,24 +17,14 @@ var results = {cheezits: 0, goldfish: 0};
 
 const authenticate = (req, res, next) => {
 	const token = req.cookies.token;
-
-
-
-	console.log(typeof uuidv4());
-
 	if(token) {
 		jwt.verify(token, TOKEN_SECRET, (err, payload) => {
 			if(err) next();
 			res.redirect('/results');
 		})
 	} else {
-		const newToken = jwt.sign({
-			id: uuidv4()
-		}, TOKEN_SECRET);
-
+		const newToken = jwt.sign({id: uuidv4()}, TOKEN_SECRET);
 		res.cookie('token', newToken, {maxAge: 1000*86400*30, httpOnly: true});
-		console.log('entered else');
-
 		next();
 	}
 }
@@ -57,16 +44,14 @@ var server = app.listen(port, function(){
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket){
-	console.log(`Someone connected: ${socket.id}`);
+	// console.log(`Someone connected: ${socket.id}`);
 
 	socket.on("vote", function(vote){
-		// console.log(`Someone voted for ${vote.choice}`);
 		if(vote.choice == 'cheezits'){
 			results.cheezits++;
 		}else if(vote.choice == 'goldfish'){
 			results.goldfish++;
 		}
-		console.log(results);
 		socket.broadcast.emit('results', {results: results});
 	})
 
